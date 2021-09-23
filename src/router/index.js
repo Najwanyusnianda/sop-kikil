fe39179a1c4d06773af2a5a87be44173500d0cd7
Najwanyusnianda <najwanyusnianda@gmail.com>
2021-09-23 17:24:44 +0700

    aa

--- src/router/index.js
+++ src/router/index.js
@@ -16,17 +16,18 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path:'/dashboard',
-    name:'Dashboard',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Dashboard.vue')
-
-  },
-  {
     path:'/sop',
     name:'Sop',
     component: () => import(/* webpackChunkName: "about" */ '../views/Sop.vue')
 
   },
+  {
+    path:'/dashboard',
+    name:'Dashboard',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Dashboard.vue')
+
+  },
+
 
   {
     path:'/management',
