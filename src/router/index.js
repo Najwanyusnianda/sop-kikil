357b987042284647dc2ff3e2659bb24c789ac299
Najwanyusnianda <najwanyusnianda@gmail.com>
2021-09-23 17:46:31 +0700

    aaa

--- src/router/index.js
+++ src/router/index.js
@@ -15,19 +15,19 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
-
-  {
-    path:'/dashboard',
-    name:'Dashboard',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Dashboard.vue')
-
-  },
   {
     path:'/sop',
     name:'Sop',
     component: () => import(/* webpackChunkName: "about" */ '../views/Sop.vue')
 
   },
+  {
+    path:'/dashboard',
+    name:'Dashboard',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Dashboard.vue')
+
+  },
+
 
   {
     path:'/management',
